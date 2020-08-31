49aefb4e36c2f876cb9a92a84258b61c43f04777
Nworm <1574242600@qq.com>
2020-08-31 17:49:31 +0800

    修改 404页 样式

--- src/layout/404.js
+++ src/layout/404.js
@@ -4,9 +4,9 @@ import { i18n as _ } from '../global';
 export default class Notfound extends React.Component {
     render() {
         return ( 
-            <div style={{textAlign: 'center', margin:'25% 0'}}>
-                <div>404</div>
-                <div>{_('404text')}</div>
+            <div style={{textAlign: 'center', margin:'20% 0'}}>
+                <div style={{fontSize: '60px', color: '#51b26d'}}>404</div>
+                <div style={{color: '#666'}}>{_('404text')}</div>
             </div>
         )
     }
