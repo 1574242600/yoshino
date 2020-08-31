e1739e5d4c8dc78d2ceae19aa05bd4d80d6f11eb
Nworm <1574242600@qq.com>
2020-08-31 17:46:25 +0800

    完成 404页

--- src/layout/404.js
+++ src/layout/404.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import * as Yoshino from 'yoshino';
-
+import { i18n as _ } from '../global';
 
 export default class Notfound extends React.Component {
     render() {
         return ( 
-            <div>
-                404
+            <div style={{textAlign: 'center', margin:'25% 0'}}>
+                <div>404</div>
+                <div>{_('404text')}</div>
             </div>
         )
     }
