75e37fdf9e1f36e3e4386de17be39681165ef628
Nworm <1574242600@qq.com>
2020-09-01 21:16:49 +0800

    修改页脚内容

--- src/layout/partial/footer.js
+++ src/layout/partial/footer.js
@@ -70,9 +70,8 @@ export default class Footer extends React.Component {
             <footer>
                 <Since />
                 <div>
-                    © { year } { siteName }. 由 <a href="https://github.com/1574242600/1574242600.github.io">undefined</a> 强力驱动.
-                    <span>Theme By <a href="https://github.com/1574242600/yoshino">yoshino v0.0.1 alpha</a></span>
-                    <span><a href="./sitemap.xml">站点地图</a></span>
+                    © { year } { siteName }. 由 <a href="https://github.com/1574242600/1574242600.github.io-spa/tree/source">undefined</a> 强力驱动.
+                    <span>Theme By <a href="https://github.com/1574242600/yoshino">yoshino v0.0.1 alpha dev</a></span>
                 </div>
                 <div>
                     Made with by { author }.
