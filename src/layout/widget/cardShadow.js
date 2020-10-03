feb94d86ea1ce9283bd4603d6180f1733d1525fb
Nworm <1574242600@qq.com>
2020-10-03 12:18:05 +0800

    feat: 添加props默认值

--- src/layout/widget/cardShadow.js
+++ src/layout/widget/cardShadow.js
@@ -5,9 +5,13 @@ const { Row } = Yoshino.Grid;
 export default class CardShadow extends React.Component {
     render() {
         return (
-            <Row style={{ backgroundColor: '#f5f5f5', margin: this.props.margin }} className={'yoshino-card-shadow'} >
+            <Row style={{ backgroundColor: '#f5f5f5', margin: this.props.margin}} className={'yoshino-card-shadow'} >
                 {this.props.children}
             </Row>
         )
     }
+}
+
+CardShadow.defaultProps = {
+    margin: '0'
 }
