449868a3b2149a661c042e9132f455272028ec32
Nworm <1574242600@qq.com>
2020-10-05 17:45:22 +0800

    feat: 友链自适应

--- src/layout/partial/linkCard.js
+++ src/layout/partial/linkCard.js
@@ -7,7 +7,7 @@ export default class LinkCard extends React.Component {
         const data = this.props.data;
 
         return (
-            <Col xs={ 6 } style={{padding: '16px '}}>
+            <Col xs={ 12 } lg={ 6 } sm={ 8} style={{padding: '16px '}}>
                 <a href={ data.url } style={{ textDecoration: 'none'}} >
                     <img src={ data.avatar } alt={ data.title } style={{width:'100px',height: '100px'}}/>
                     <h3 style={{color: '#555', fontSize: '16px'}} >{ data.name }</h3>
